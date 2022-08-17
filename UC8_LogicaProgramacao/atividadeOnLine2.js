b88611a39ba52cb31fd1ed02112719596af44c09
Sergio do Nascimento <gio_nas@hotmail.com>
2022-08-17 15:53:33 -0300

    Atividade OnLine 2
    Commit Atividade OnLine 2

---
+++ UC8_LogicaProgramacao/atividadeOnLine2.js
@@ -0,0 +1,23 @@
+// Exemplo de controle par ou impar para classificação de alunos
+
+let alunos = 15;
+
+for (let contador = 0; contador < alunos; contador++) {
+   
+    console.log(contador);
+
+    if (contador == 0) {
+        
+        console.log('o número é o zero')
+    }
+    if (contador % 2 == 0 && contador != 0) {
+        
+        console.log('o número é par')
+
+    }
+    if (contador % 2 != 0) {
+
+        console.log('o número é ímpar')
+
+    }
+}
