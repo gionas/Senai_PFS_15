fc424054bd099b5153b79b53a91cfed60a90fa0b
Sergio do Nascimento <gio_nas@hotmail.com>
2022-08-17 13:39:58 -0300

    Encontros Remotos
    Atividades !!!

---
+++ UC8_LogicaProgramacao/encontroRemoto01.js
@@ -0,0 +1,31 @@
+// Econtro Remoto 01
+
+console.log ('******************************')
+let peso = 150
+let nomePeca = 'Amortecedor'
+
+if (peso>100) {
+    console.log('Realizar Cadastro')    
+} else {
+    console.log('Cadastro Negado, peça não atende os requisitos')         
+} 
+console.log ('******************************\n')
+
+console.log ('******************************')
+let listaPecas = 10
+
+
+if (listaPecas>10){
+    console.log('Caixa excede o limite')
+} else {
+    console.log('Cadastro Permitido')
+}
+console.log ('******************************\n')
+
+console.log ('******************************')
+if (nomePeca.length >3) {
+    console.log('Realize Cadastro')
+} else {
+    console.log('Nome inválido')
+}
+console.log ('******************************\n')
---
+++ UC8_LogicaProgramacao/encontroRemoto02.js
@@ -0,0 +1,22 @@
+console.log ('******************************\n')
+
+let numeroDeAlunos = 10;
+
+for (let contador = 0; contador < numeroDeAlunos; contador++) {
+    console.log(contador)
+
+    if (contador == 0) {
+        console.log('classificado o Zero')
+    }
+
+    if (contador %2 == 0) {
+        console.log('Número Par')
+    } else {
+        console.log('Número Impar')
+    }
+
+}
+
+console.log ('******************************')
+
+
