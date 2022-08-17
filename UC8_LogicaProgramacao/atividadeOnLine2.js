66acd32f4fb83c83b42b3158d37e75f7141879eb
Sergio do Nascimento <gio_nas@hotmail.com>
2022-08-17 16:13:20 -0300

    Melhorias no Código
    atualização

--- UC8_LogicaProgramacao/atividadeOnLine2.js
+++ UC8_LogicaProgramacao/atividadeOnLine2.js
@@ -1,6 +1,6 @@
 // Exemplo de controle par ou impar para classificação de alunos
 
-let alunos = 15;
+let alunos = 20;
 
 for (let contador = 0; contador < alunos; contador++) {
    
@@ -8,16 +8,16 @@ for (let contador = 0; contador < alunos; contador++) {
 
     if (contador == 0) {
         
-        console.log('o número é o zero')
+        console.log('O número é o zero')
     }
     if (contador % 2 == 0 && contador != 0) {
         
-        console.log('o número é par')
+        console.log('O número é par')
 
     }
     if (contador % 2 != 0) {
 
-        console.log('o número é ímpar')
+        console.log('O número é ímpar')
 
     }
 }
